3aac148f095aae887f19b5d41df58b9d2d87e5ba
John Torres <john.torres.hurtado@gmail.com>
2019-03-28 09:50:16 -0700

    added horizontal ruler

--- js/expand.js
+++ js/expand.js
@@ -1,18 +1,18 @@
 
-pc.map(a =>{
-  $('aside').append(`
-    ${a.title ? `
-    <details>
-      <summary>${a.title}</summary>
-      <div class="sum-secondary-menu">
-        ${a.level2.map(
-        b => `<div class="nav-item" hcd> <a>${b}</a></div>`).join('')}
-      </div>
-    </details>` :
-
-    a.single ?
-    `<div class="nav-item" hcd>
-      <a>${a.single}</a>
-    </div>` : `<hr></hr>`
-  }`)
-})
+// pc.map(a =>{
+//   $('aside').append(`
+//     ${a.title ? `
+//     <details>
+//       <summary>${a.title}</summary>
+//       <div class="sum-secondary-menu">
+//         ${a.level2.map(
+//         b => `<div class="nav-item" hcd> <a>${b}</a></div>`).join('')}
+//       </div>
+//     </details>` :
+//
+//     a.single ?
+//     `<div class="nav-item" hcd>
+//       <a>${a.single}</a>
+//     </div>` : `<hr></hr>`
+//   }`)
+// })
